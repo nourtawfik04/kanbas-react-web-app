109c96dfb66f49312f2f81c2d0b1290a05f53153
nourtawfik04 <tawfik.n@northeastern.edu>
2024-10-16 21:26:12 -0400

    a3 updates

--- src/Kanbas/Dashboard.tsx
+++ src/Kanbas/Dashboard.tsx
@@ -1,51 +1,51 @@
 import { Link } from "react-router-dom";
+import { courses } from "./Database";
 
 export default function Dashboard() {
-  const courses = [
-    {
-      id: 1234,
-      title: "React JS",
-      term: "Full Stack software developer",
-      imgSrc: "/images/c2.png",
-    },
-    {
-      id: 1235,
-      title: "Algorithms",
-      term: "MLA Researcher",
-      imgSrc: "/images/c2.png",
-    },
-    {
-      id: 1236,
-      title: "OOD",
-      term: "Software Engineer",
-      imgSrc: "/images/c3.png",
-    },
-    {
-      id: 1237,
-      title: "Database Design",
-      term: "Data Analyst",
-      imgSrc: "/images/c4.png",
-    },
-    {
-      id: 1238,
-      title: "Fundies",
-      term: "Back-End Software Engineer",
-      imgSrc: "/images/c5.png",
-    },
-    {
-      id: 1239,
-      title: "Co-op Class",
-      term: "Product Designer",
-      imgSrc: "/images/c6.png",
-    },
-    {
-      id: 1240,
-      title: "Discrete Structures",
-      term: "Electrical Engineer",
-      imgSrc: "/images/c6.png",
-    },
-  ];
-
+  // const courses = [
+  //   {
+  //     id: 1234,
+  //     title: "React JS",
+  //     term: "Full Stack software developer",
+  //     imgSrc: "/images/c2.png",
+  //   },
+  //   {
+  //     id: 1235,
+  //     title: "Algorithms",
+  //     term: "MLA Researcher",
+  //     imgSrc: "/images/c2.png",
+  //   },
+  //   {
+  //     id: 1236,
+  //     title: "OOD",
+  //     term: "Software Engineer",
+  //     imgSrc: "/images/c3.png",
+  //   },
+  //   {
+  //     id: 1237,
+  //     title: "Database Design",
+  //     term: "Data Analyst",
+  //     imgSrc: "/images/c4.png",
+  //   },
+  //   {
+  //     id: 1238,
+  //     title: "Fundies",
+  //     term: "Back-End Software Engineer",
+  //     imgSrc: "/images/c5.png",
+  //   },
+  //   {
+  //     id: 1239,
+  //     title: "Co-op Class",
+  //     term: "Product Designer",
+  //     imgSrc: "/images/c6.png",
+  //   },
+  //   {
+  //     id: 1240,
+  //     title: "Discrete Structures",
+  //     term: "Electrical Engineer",
+  //     imgSrc: "/images/c6.png",
+  //   },
+  // ];
   return (
     <div id="wd-dashboard" className="container-fluid">
       <h1 id="wd-dashboard-title" className="text-center my-4">
@@ -63,29 +63,27 @@ export default function Dashboard() {
           {courses.map((course) => (
             <div
               className="wd-dashboard-course col"
-              key={course.id}
+              key={course._id} 
               style={{ width: "270px", marginBottom: "30px" }}
             >
               <div className="card rounded-3 overflow-hidden shadow-sm">
                 <Link
                   className="wd-dashboard-course-link text-decoration-none text-dark"
-                  to={`/Kanbas/Courses/${course.id}/Home`}
+                  to={`/Kanbas/Courses/${course._id}/Home`}
                 >
- 
                   <img
-                    src={course.imgSrc}
+                    src={course.imgSrc || "/images/c4.png"} 
                     width="100%"
                     height={160}
-                    alt={course.title}
+                    alt={course.name} 
                   />
                   <div className="card-body text-center">
-
                     <h5 className="wd-dashboard-course-title card-title">
-                      {course.title}
+                      {course.name}
                     </h5>
-      
+
                     <p className="wd-dashboard-course-title card-text">
-                      {course.term}
+                      {course.description}
                     </p>
                     <button className="btn btn-primary">Go</button>
                   </div>
--- src/Kanbas/Navigation.tsx
+++ src/Kanbas/Navigation.tsx
@@ -1,10 +1,17 @@
 import { AiOutlineDashboard } from "react-icons/ai";
-import { CiCalendar, CiSettings } from "react-icons/ci";
+import { IoCalendarOutline } from "react-icons/io5";
+import { LiaBookSolid, LiaCogSolid } from "react-icons/lia";
 import { FaInbox, FaRegCircleUser } from "react-icons/fa6";
-import { LiaBookSolid } from "react-icons/lia";
-import { NavLink } from "react-router-dom";
-
+import { Link, useLocation } from "react-router-dom";
 export default function KanbasNavigation() {
+  const { pathname } = useLocation();
+  const links = [
+    { label: "Dashboard", path: "/Kanbas/Dashboard", icon: AiOutlineDashboard },
+    { label: "Courses", path: "/Kanbas/Dashboard", icon: LiaBookSolid },
+    { label: "Calendar", path: "/Kanbas/Calendar", icon: IoCalendarOutline },
+    { label: "Inbox", path: "/Kanbas/Inbox", icon: FaInbox },
+    { label: "Labs", path: "/Labs", icon: LiaCogSolid },
+  ];
   return (
     <div
       id="wd-kanbas-navigation"
@@ -17,87 +24,41 @@ export default function KanbasNavigation() {
         href="https://www.northeastern.edu/"
         className="list-group-item bg-black border-0 text-center"
       >
-        <img
-          src="/images/northeastern.svg"
-          width="75px"
-          alt="Northeastern University"
-        />
+        <img src="/images/northeastern.svg" width="75px" />
       </a>
-      <NavLink
+      <Link
         to="/Kanbas/Account"
-        id="wd-account-link"
-        className={({ isActive }) =>
-          isActive
-            ? "list-group-item text-center border-0 bg-white text-danger"
-            : "list-group-item bg-black text-center border-0 text-white"
-        }
+        className={`list-group-item text-center border-0 bg-black
+            ${
+              pathname.includes("Account")
+                ? "bg-white text-danger"
+                : "bg-black text-white"
+            }`}
       >
-        <FaRegCircleUser className="fs-1" />
+        <FaRegCircleUser
+          className={`fs-1 ${
+            pathname.includes("Account") ? "text-danger" : "text-white"
+          }`}
+        />
+        <br />
         Account
-      </NavLink>
-      <NavLink
-        to="/Kanbas/Dashboard"
-        id="wd-dashboard-link"
-        className={({ isActive }) =>
-          isActive
-            ? "list-group-item text-center border-0 bg-white text-danger"
-            : "list-group-item text-center border-0 bg-black text-white"
-        }
-      >
-        <AiOutlineDashboard className="fs-1 text-danger" />
-        Dashboard
-      </NavLink>
-      <NavLink
-        to="/Kanbas/Courses/Assignments"
-        id="wd-course-link"
-        className={({ isActive }) =>
-          isActive
-            ? "list-group-item text-center border-0 bg-white text-danger"
-            : "list-group-item text-center border-0 bg-black text-white"
-        }
-      >
-        <LiaBookSolid className="fs-1 text-danger" />
-        Courses
-      </NavLink>
-      <NavLink
-        to="/Kanbas/Calendar"
-        id="wd-calendar-link"
-        className={({ isActive }) =>
-          isActive
-            ? "list-group-item text-center border-0 bg-white text-danger"
-            : "list-group-item text-center border-0 bg-black text-white"
-        }
-      >
-        <CiCalendar className="fs-1 text-danger" />
-        <br />
-        Calendar
-      </NavLink>
-      <NavLink
-        to="/Kanbas/Inbox"
-        id="wd-inbox-link"
-        className={({ isActive }) =>
-          isActive
-            ? "list-group-item text-center border-0 bg-white text-danger"
-            : "list-group-item text-center border-0 bg-black text-white"
-        }
-      >
-        <FaInbox className="fs-1 text-danger" />
-        <br />
-        Inbox
-      </NavLink>
-      <NavLink
-        to="/#/Labs/Lab1"
-        id="wd-labs-link"
-        className={({ isActive }) =>
-          isActive
-            ? "list-group-item text-center border-0 bg-white text-danger"
-            : "list-group-item text-center border-0 bg-black text-white"
-        }
-      >
-        <CiSettings className="fs-1 text-danger" />
-        <br />
-        Labs
-      </NavLink>
+      </Link>
+      {links.map((link) => (
+        <Link
+          key={link.path}
+          to={link.path}
+          className={`list-group-item bg-black text-center border-0
+              ${
+                pathname.includes(link.label)
+                  ? "text-danger bg-white"
+                  : "text-white bg-black"
+              }`}
+        >
+          {link.icon({ className: "fs-1 text-danger" })}
+          <br />
+          {link.label}
+        </Link>
+      ))}
     </div>
   );
 }
